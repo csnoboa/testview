1c4203565c0e6fb3147d6e6b94491c29b4d8d5f8
csnoboa <caique2noboa@gmail.com>
2021-03-19 21:56:51 -0300

    add folder backend

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+MongoDBConnect = "localhost:27017"
+FILE_EXTENSIONS_PERMITED = [".py", ".java", ".kv", ".dart", ".js"]
+DAYS_DELTA = 60
